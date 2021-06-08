ceac72a53d22da85dfb098b33fd1eaf6631dc471
FlorentinTh <florentin.thullier1@uqac.ca>
2021-06-08 12:08:57 -0400

    fix(postprocessing): remove no longer required postProcessing class

--- src/postProcessing.js
+++
@@ -1,7 +0,0 @@
-class PostProcessing {
-  constructor(serverName) {
-    this.serverName = serverName.toLowerCase().split(' ').join('_');
-  }
-}
-
-export default PostProcessing;
--- src/sanitizer.js
+++ src/sanitizer.js
@@ -1,14 +1,20 @@
-import PostProcessing from './postProcessing';
 import { Tags, ConsoleHelper } from './helpers/consoleHelper';
 
-class Sanitizer extends PostProcessing {
-  constructor(serverName) {
-    if (serverName === null) {
-      ConsoleHelper.printMessage(Tags.ERROR, `serverName cannot be null`);
+class Sanitizer {
+  format(server, message) {
+    if (!Object.prototype.toString.call(server) === '[object String]') {
+      ConsoleHelper.printMessage(Tags.ERROR, `server parameter must be a string`);
       process.exit(1);
     }
 
-    super(serverName);
+    if (!Object.prototype.toString.call(message) === '[object String]') {
+      ConsoleHelper.printMessage(Tags.ERROR, `message parameter must be a string`);
+      process.exit(1);
+    }
+
+    if (server === 'rfid' || server === 'energetic' || server === 'sensors') {
+      return `{"data": ${message}}`;
+    }
   }
 }
 
